fed2149eca4709baa458a311e44be16aac81d919
Ignacio Benedetti <ignacio.benedetti@cyclelogic.com>
2011-02-02 18:43:49 -0300

    Vuelta a 0

--- lib/constants.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-STRING = (str, unicode)
-NUMBER = (int, long)
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
--- lib/pidlocks.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-def lock_pid(pid_path):
-    if os.path.exists(pid_path):
-        return False
-
-    open(pid_path, 'w').write(str(os.getpid()))
-    return True
